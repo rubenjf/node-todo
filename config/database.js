99ab74a6d3aaa6a9c9bc2771ac19bbae17bcefcd
Ruben Fernando <ruben.fernando@hp.com>
2014-08-14 10:54:39 +0530

    the very first commit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+	// the database url to connect
+	//url : 'mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'
+	//url : 'mongodb:rubenjf:happy123@proximus.modulusmongo.net:27017/xaG2ubub'
+	url : 'mongodb://localhost:27017/mydb'
+	
+}
